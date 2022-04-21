734f741a080b5ba4ee43fa5ed2cdc8b35dd257e6
lelandbove27 <96493631+lelandbove27@users.noreply.github.com>
2022-04-20 17:20:01 -0700

    Update botTest.js

--- botTest.js
+++ botTest.js
@@ -1,16 +1,17 @@
 const Discord = require("discord.js")
 const { Client, Intents } = require('discord.js');
 const client = new Discord.Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+require('dotenv').config();
 
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-});
+})
 
 client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("pong");
   }
-})
+});
 
 client.login(process.env.TOKEN);
