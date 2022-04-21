a8ff9c43e607be9e364a9ca435877a8c8050cbf0
lelandbove27 <96493631+lelandbove27@users.noreply.github.com>
2022-04-21 11:51:44 -0700

    Update botTest.js

--- botTest.js
+++ botTest.js
@@ -1,3 +1,6 @@
+//npm install discord.js
+//npm install dotenv
+
 const Discord = require("discord.js")
 const { Client, Intents } = require('discord.js');
 const client = new Discord.Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
